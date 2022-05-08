bd854bd9f22e121a076bbcd68f5ef20643558b58
Florante Gamat <yonggamat@gmail.com>
2022-05-08 19:52:25 +0800

    Initial Commit: Setup Spring Boot Application

---
+++ src/main/java/com/github/yonggamat/eventdriven/EventDrivenSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.yonggamat.eventdriven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/yonggamat/eventdriven/EventDrivenSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yonggamat.eventdriven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
